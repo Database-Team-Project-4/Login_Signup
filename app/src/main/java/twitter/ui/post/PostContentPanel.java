e6af69a4466c86430be12294ef2839ecebee45dd
jaekyoung choi <chlworud0722@gahon.ac.kr>
2024-11-27 15:04:14 +0900

    post image

--- app/src/main/java/twitter/ui/post/PostContentPanel.java
+++ app/src/main/java/twitter/ui/post/PostContentPanel.java
@@ -3,21 +3,14 @@ package twitter.ui.post;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.util.List;
-import javax.imageio.ImageIO;
 
 public class PostContentPanel extends JPanel {
     public PostContentPanel(String contentText, String postInfo, int likes, int comments, int bookmarks, List<byte[]> images) {
-    int IMAGE_WIDTH = 300; // 고정된 이미지 너비
-    int IMAGE_HEIGHT = 200; // 고정된 이미지 높이
+        // BoxLayout을 Y_AXIS로 설정하여 수직으로 컴포넌트 배치
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBackground(Color.BLACK);
 
         // 본문 텍스트 설정
-
-        // 본문 내용
         JTextArea contentArea = new JTextArea(contentText);
         contentArea.setEditable(false);
         contentArea.setBackground(Color.BLACK);
@@ -69,32 +62,6 @@ public class PostContentPanel extends JPanel {
         postInfoLabel.setAlignmentX(Component.LEFT_ALIGNMENT); // 왼쪽 정렬
 
         // 작성 날짜 및 조회수 패널에 추가
-        // 이미지 추가 (가운데 정렬)
-        if (images != null && !images.isEmpty()) {
-            for (byte[] imageBytes : images) {
-                try {
-                    BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageBytes));
-                    if (img != null) {
-                        Image scaledImg = img.getScaledInstance(IMAGE_WIDTH, IMAGE_HEIGHT, Image.SCALE_SMOOTH);
-                        ImageIcon icon = new ImageIcon(scaledImg);
-
-                        // 이미지를 감싸는 패널 생성
-                        JPanel imagePanel = new JPanel(new FlowLayout(FlowLayout.CENTER)); // 이미지 가운데 정렬
-                        imagePanel.setBackground(Color.BLACK); // 배경색 설정
-                        JLabel imageLabel = new JLabel(icon);
-                        imagePanel.add(imageLabel);
-
-                        // 이미지 패널 추가
-                        add(imagePanel);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        // 패널에 추가
-        add(contentArea);
         add(postInfoLabel);
     }
 }
